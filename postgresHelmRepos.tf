459bd3ccb100c9ff8150621d527b285383fb017b
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-06-28 23:38:18 +0200

    add helm repos database

---
+++ postgresHelmRepos.tf
@@ -0,0 +1,30 @@
+
+resource "random_string" "helm-repositories" {
+  length  = 32
+  special = false
+}
+
+resource "postgresql_role" "helm-repositories" {
+  name     = "helm-repositories"
+  login    = true
+  password = "${random_string.helm-repositories.result}"
+}
+resource "postgresql_database" "helm-repositories" {
+  name              = "helm-repositories"
+  owner             = "${postgresql_role.helm-repositories.name}"
+  allow_connections = true
+}
+
+
+resource "kubernetes_secret" "helm-repositories" {
+  type = "Opaque"
+
+  metadata {
+    name      = "database-uri"
+    namespace = "${kubernetes_namespace.openfaas-fn.metadata.0.name}"
+  }
+
+  data = {
+    database-uri = "postgresql://${postgresql_role.helm-repositories.name}:${random_string.helm-repositories.result}@${digitalocean_database_cluster.postgres.host}:${digitalocean_database_cluster.postgres.port}/${postgresql_database.helm-repositories.name}"
+  }
+}
