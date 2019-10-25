74c77348e3cd4e23ae866bdd7d86abd994db3711
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-10-25 16:58:57 +0200

    add database

--- postgres.tf
+++ postgres.tf
@@ -1,10 +1,10 @@
-# provider "postgresql" {
-#   host            = "${digitalocean_database_cluster.postgres.host}"
-#   port            = "${digitalocean_database_cluster.postgres.port}"
-#   database        = "${digitalocean_database_cluster.postgres.database}"
-#   username        = "${digitalocean_database_cluster.postgres.user}"
-#   password        = "${digitalocean_database_cluster.postgres.password}"
-#   sslmode         = "require"
-#   connect_timeout = 15
-#   superuser = false
-# }
+provider "postgresql" {
+  host            = "${digitalocean_database_cluster.postgres.host}"
+  port            = "${digitalocean_database_cluster.postgres.port}"
+  database        = "${digitalocean_database_cluster.postgres.database}"
+  username        = "${digitalocean_database_cluster.postgres.user}"
+  password        = "${digitalocean_database_cluster.postgres.password}"
+  sslmode         = "require"
+  connect_timeout = 15
+  superuser = false
+}
--- postgresHelmNotifier.tf
+++ postgresHelmNotifier.tf
@@ -1,30 +1,30 @@
 
-# resource "random_string" "helm-notifier" {
-#   length  = 32
-#   special = false
-# }
+resource "random_string" "helm-notifier" {
+  length  = 32
+  special = false
+}
 
-# resource "postgresql_role" "helm-notifier" {
-#   name     = "helm-notifier"
-#   login    = true
-#   password = "${random_string.helm-repositories.result}"
-# }
-# resource "postgresql_database" "helm-notifier" {
-#   name              = "helm-notifier"
-#   owner             = "${postgresql_role.helm-repositories.name}"
-#   allow_connections = true
-# }
+resource "postgresql_role" "helm-notifier" {
+  name     = "helm-notifier"
+  login    = true
+  password = "${random_string.helm-repositories.result}"
+}
+resource "postgresql_database" "helm-notifier" {
+  name              = "helm-notifier"
+  owner             = "${postgresql_role.helm-repositories.name}"
+  allow_connections = true
+}
 
 
-# resource "kubernetes_secret" "helm-notifier" {
-#   type = "Opaque"
+resource "kubernetes_secret" "helm-notifier" {
+  type = "Opaque"
 
-#   metadata {
-#     name      = "database-uri"
-#     namespace = "${kubernetes_namespace.openfaas-fn.metadata.0.name}"
-#   }
+  metadata {
+    name      = "database-uri"
+    namespace = "${kubernetes_namespace.openfaas-fn.metadata.0.name}"
+  }
 
-#   data = {
-#     database-uri = "postgresql://${postgresql_role.helm-repositories.name}:${random_string.helm-repositories.result}@${digitalocean_database_cluster.postgres.host}:${digitalocean_database_cluster.postgres.port}/${postgresql_database.helm-repositories.name}"
-#   }
-# }
+  data = {
+    database-uri = "postgresql://${postgresql_role.helm-repositories.name}:${random_string.helm-repositories.result}@${digitalocean_database_cluster.postgres.host}:${digitalocean_database_cluster.postgres.port}/${postgresql_database.helm-repositories.name}"
+  }
+}
