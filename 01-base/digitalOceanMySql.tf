4e3e454d33e61ebcd0e99ced8b6a4d59dafe0988
Pierre Humberdroz <p.humberdroz@gmail.com>
2020-01-16 20:41:55 +0100

    update database name

--- 01-base/digitalOceanMySql.tf
+++ 01-base/digitalOceanMySql.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_database_cluster" "mysql" {
-  name       = "01-mysql"
+  name       = "zero-one-mysql"
   engine     = "mysql"
   size       = "db-s-1vcpu-1gb"
   region     = "fra1"
