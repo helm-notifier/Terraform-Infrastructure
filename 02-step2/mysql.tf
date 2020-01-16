d7a620a51a19b887bd9e0c8de7d6c8ae6233bff5
Pierre Humberdroz <p.humberdroz@gmail.com>
2020-01-16 23:59:34 +0100

    endpoint with port

--- 02-step2/mysql.tf
+++ 02-step2/mysql.tf
@@ -1,5 +1,5 @@
 provider "mysql" {
-  endpoint = data.digitalocean_database_cluster.mysql.host
+  endpoint = "${data.digitalocean_database_cluster.mysql.host}:${data.digitalocean_database_cluster.mysql.port}"
   username = data.digitalocean_database_cluster.mysql.user
   password = data.digitalocean_database_cluster.mysql.password
 }
