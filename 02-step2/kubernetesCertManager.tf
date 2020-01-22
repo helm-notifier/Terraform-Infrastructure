d9589369e475653b20781132a393734730b3291b
Pierre Humberdroz <p.humberdroz@gmail.com>
2020-01-22 05:41:11 +0100

    deploy token for cert manager

--- 02-step2/kubernetesCertManager.tf
+++ 02-step2/kubernetesCertManager.tf
@@ -3,3 +3,14 @@ resource "kubernetes_namespace" "certManager" {
     name = "cert-manager"
   }
 }
+
+resource "kubernetes_secret" "cert-manager" {
+  type = "Opaque"
+  metadata {
+    name = "cert-manager"
+    namespace = kubernetes_namespace.certManager.metadata.0.name
+  }
+  data = {
+    access-token = var.do_token
+  }
+}
